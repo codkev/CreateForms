b1d9fe1e4cd5fbb6ac67f2ee7fddafa85d0117db
kevin calizaya <kevcalizaya@gmail.com>
2021-07-02 23:15:44 -0400

    se cambio de nombre el modulo a RenderFormNode y el modulo se mejor para renderizar radiobuttos,checkbx y los demas tipos de inputs

--- scripts.js
+++ scripts.js
@@ -1,16 +1,46 @@
-import RenderFormNode from "./RenderForms.js";
+import RenderFormNode from "./RenderFormNode.js";
 
-const $app=document.getElementById("app");
+const $app=document.getElementById("app").querySelector("form");
 
-let $node=RenderFormNode({
-    name:"nombre",
-    type:"text",
-    label:"Ingresa tu nombre",
-    placeholder:"Ingresa tu nombre",
-})
+
 let $node2=RenderFormNode({
-    name:"celular",
-    type:"date",
-    label:"Ingresa tu cumple",
-})
+    name:"lenguajes web",
+    type:"checkbox",
+    label:"Selecciona un lenguaje <br>",
+    options:[
+        {
+            value:"css",
+            text:"css"
+        },
+        {
+            value:"JavaCript",
+            text:"JavaCript"
+        },
+        {
+            value:"html",
+            text:"html"
+        },
+    ]
+}),
+pregunta1=RenderFormNode({
+    name:"edad",
+    type:"radio",
+    label:"<br>Elija su edad<br>",
+    options:[
+        {
+            value:"0-10",
+        },
+        {
+            value:"11-18",
+        }
+    ]
+}),
+butoon=RenderFormNode(
+    {
+        name:"enviar",
+        type:"submit"
+    }
+);
 $app.appendChild($node2);
+$app.appendChild(pregunta1);
+$app.appendChild(butoon);

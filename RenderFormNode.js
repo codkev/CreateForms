32dbcaf995f5ba7e6e0a5c1417d61c1fa812c1c6
kevin calizaya <kevcalizaya@gmail.com>
2021-07-08 15:34:46 -0400

    Fin de la primera version del modulo renderForm

--- RenderFormNode.js
+++ RenderFormNode.js
@@ -1,54 +1,84 @@
 //Module RenderFormNode render the inputs,selects,check,etc of the form
+//funtions
+const d = document;
+function capitalize(word) {
+  return word[0].toUpperCase() + word.slice(1);
+}
 export default function RenderFormNode(props) {
-  const d = document;
-
-  //funtions
-  function capitalize(word) {
-    return word[0].toUpperCase() + word.slice(1);
-  }
 
   //definition of necessary variables and create nodes
-  const { name, type, label, options, placeholder } = props,
+  const { name, type, label, placeholder,  value, options } = props,
     $fragment = d.createDocumentFragment(),
     $labelNodeForm = d.createElement("label"),
     $noInputs = ["select", "textarea"],
     $groupElemets = ["radio", "checkbox"];
+  // validations 
+  if(!name){
+    var error = new Error('name is undefined, name is required');
+    console.error(error);
+    return d.createTextNode("Error: name is undefined");
+  }
+
+
   //Node defined with led to create the type of element later
   let $nodeForm = null;
 
   //assign attributes to label and add it to the Fragment
-  $labelNodeForm.innerHTML = label?label:name;
+  $labelNodeForm.innerHTML = label ? label : name;
   $labelNodeForm.setAttribute("for", $groupElemets.includes(type) ? "" : name);
   $fragment.appendChild($labelNodeForm);
 
   //creation the node, assign attributes and add it to the Fragment
-  console.log(options);
+  //create of elements no inputs
   if ($noInputs.includes(type)) {
     $nodeForm = d.createElement(type);
     $nodeForm.name = name;
     $nodeForm.id = name;
-  } else {
-    if ($groupElemets.includes(type)) {
-        $nodeForm=d.createDocumentFragment();
+    $nodeForm.placeholder=placeholder||"";
+    if (type === "textarea") {
+      if(options){
+        $nodeForm.rows=options.rows||options[0];
+        $nodeForm.cols=options.cols||options[0];
+      }
+    }
+    if (type === "select") {
       options.forEach((option) => {
+        let optValue = option.value || option.toLowerCase(),
+          optText = option.text || capitalize(optValue),
+          $option = d.createElement("option");
+
+        $option.value = optValue;
+        $option.innerHTML = optValue;
+        $nodeForm.appendChild($option);
+      });
+    }
+  } else {
+    //create of checkboxs and radiobuttons "groupElements"
+    if ($groupElemets.includes(type)) {
+      $nodeForm = d.createDocumentFragment();
+      options.forEach((option) => {
+        let optValue = option.value || option.toLowerCase(),
+          optText = option.text || capitalize(optValue),
+          optId = option.id || optValue;
+
         const $labelOption = d.createElement("label"),
           $nodeOption = d.createElement("input");
-        $labelOption.setAttribute("for", option.value);
-        $labelOption.innerHTML = option.text
-          ? option.text
-          : capitalize(option.value);
+        $labelOption.setAttribute("for", optId);
+        $labelOption.innerHTML = optText;
 
         $nodeOption.type = type;
         $nodeOption.name = name;
-        $nodeOption.id = option.value;
-        $nodeOption.value = option.value;
+        $nodeOption.id = optId;
+        $nodeOption.value = optValue;
 
         $nodeForm.appendChild($labelOption);
         $nodeForm.appendChild($nodeOption);
       });
     } else {
+      //render of elemets input
       $nodeForm = d.createElement("input");
-      $nodeForm.type = type ? type : "text";
+      $nodeForm.type = type || "text";
+      $nodeForm.value = value || "";
       if (placeholder) $nodeForm.placeholder = placeholder;
     }
   }
--- scripts.js
+++ scripts.js
@@ -1,46 +1,74 @@
 import RenderFormNode from "./RenderFormNode.js";
 
-const $app=document.getElementById("app").querySelector("form");
+const $app = document.getElementById("app").querySelector("form");
 
-
-let $node2=RenderFormNode({
-    name:"lenguajes web",
-    type:"checkbox",
-    label:"Selecciona un lenguaje <br>",
-    options:[
-        {
-            value:"css",
-            text:"css"
-        },
-        {
-            value:"JavaCript",
-            text:"JavaCript"
-        },
-        {
-            value:"html",
-            text:"html"
-        },
-    ]
-}),
-pregunta1=RenderFormNode({
-    name:"edad",
-    type:"radio",
-    label:"<br>Elija su edad<br>",
-    options:[
-        {
-            value:"0-10",
-        },
-        {
-            value:"11-18",
-        }
-    ]
-}),
-butoon=RenderFormNode(
-    {
-        name:"enviar",
-        type:"submit"
-    }
-);
+let $node2 = RenderFormNode({
+    name: "lenguajes web",
+    type: "checkbox",
+    label: "Selecciona un lenguaje <br>",
+    options: [
+      {
+        value: "css",
+        text: "css",
+        id: "checkCss",
+      },
+      {
+        value: "JavaCript",
+        text: "JavaCript",
+        id: "checkJs",
+      },
+      {
+        value: "html",
+        text: "html",
+        id: "checkHtlm",
+      },
+    ],
+  }),
+  pregunta1 = RenderFormNode({
+    type: "radio",
+    label: "<br>Lenguajes Web<br>",
+    options: [
+      "Css",
+      "Html",
+      "JavaScritp",
+      {
+        value: "react",
+        text: "React js",
+      },
+    ],
+  }),
+  pregunta2 = RenderFormNode({
+    name: "nombre",
+    label: "<br>Escribe tu nombre<br>",
+    value:"kevin",
+    placeholder:"Ej: Juan perez"
+  }),
+  select = RenderFormNode({
+    name: "mes",
+    type: "select",
+    label: "<br>Selecione un mes<br>",
+    placeholder:"Selecciona un mes",
+    options: ["Enero", "Febrero", "Marzo", "Junio", "Julio"],
+  }),
+  range = RenderFormNode({
+    name: "animo",
+    type: "range",
+    value: 10,
+  }),
+  comentario = RenderFormNode({
+    name: "comentario",
+    type: "textarea",
+    placeholder:"Deja un comentario",
+    label: "<br>Ingresa un comentario<br>",
+  }),
+  butoon = RenderFormNode({
+    name: "enviar",
+    type: "submit",
+  });
 $app.appendChild($node2);
 $app.appendChild(pregunta1);
+$app.appendChild(pregunta2);
+$app.appendChild(select);
+$app.appendChild(comentario);
+$app.appendChild(range);
 $app.appendChild(butoon);
